b435c78b09eb5d57a8f08717d7751e1caa8c8399
yj <liuwu4@yeah.net>
2021-01-05 15:02:42 +0800

    build: 测试push build

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-import React from 'react';
+import React from "react";
 
 class Index extends React.Component {
-    render() {
-        return (
-            <div>
-                类组件
-            </div>
-        )
-    }
+  render() {
+    return <div>434</div>;
+  }
 }
 export default Index;
